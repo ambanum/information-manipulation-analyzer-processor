bfd9fc0d835d1d602d73e4f4c05e0c430b122add
Martin Ratinaud <martinratinaud@gmail.com>
2021-03-18 17:27:32 +0400

    chore: add better logs with time to see how long a command lasts

--- src/common/logging.ts
+++ src/common/logging.ts
@@ -1,7 +1,15 @@
 import debugLib from 'debug';
 
-export const debug = debugLib('ima:debug');
+const getTime = () => {
+  const date = new Date();
+  return `${date.getHours().toString().padStart(2, '0')}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+};
+
+export const debug = (...args: any[]) => debugLib('ima:debug')(`[${getTime()}]`, ...args);
 // spaces are intended to align correclty logs in console
-export const info = debugLib('ima: info');
-export const warn = debugLib('ima: warn');
-export const error = debugLib('ima: error');
+export const info = (...args: any[]) => debugLib('ima: info')(`[${getTime()}]`, ...args);
+export const warn = (...args: any[]) => debugLib('ima: warn')(`[${getTime()}]`, ...args);
+export const error = (...args: any[]) => debugLib('ima: error')(`[${getTime()}]`, ...args);
