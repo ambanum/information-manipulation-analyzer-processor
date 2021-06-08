5edc91f0118e0a0a2f311780126d85d358e39bb1
Martin Ratinaud <martinratinaud@gmail.com>
2021-06-08 16:04:13 +0200

    chore: refactor poller to be able to poll usernames afterwards

---
+++ src/hashtags.ts
@@ -0,0 +1,197 @@
+import * as HashtagVolumetryManager from 'managers/HashtagVolumetryManager';
+import * as ProcessorManager from 'managers/ProcessorManager';
+import * as logging from 'common/logging';
+
+import { HashtagStatuses, QueueItemActionTypes, QueueItemStatuses } from 'interfaces';
+
+import QueueItemManager from 'managers/QueueItemManager';
+import Scraper from 'common/node-snscrape';
+
+const WAIT_TIME = 1 * 1000; // 1s
+const NB_TWEETS_TO_SCRAPE = process.env?.NB_TWEETS_TO_SCRAPE;
+const MIN_PRIORITY = parseInt(process.env?.MIN_PRIORITY || '0', 10);
+const NEXT_PROCESS_IN_FUTURE = 60 * 60 * 1000;
+const logPrefix = '[hashtag]';
+
+// Because I could not find other way in tyepscript to get functions of a class
+type Properties<T> = { [K in keyof T]: T[K] };
+export default class HashtagPoller {
+  private processorId: string;
+  private logger: typeof logging;
+  private queueItemManager: QueueItemManager;
+
+  constructor({ processorId }) {
+    this.processorId = processorId;
+    this.logger = {
+      debug: (...args: any[]) => logging.debug(logPrefix, ...args),
+      info: (...args: any[]) => logging.info(logPrefix, ...args),
+      warn: (...args: any[]) => logging.warn(logPrefix, ...args),
+      error: (...args: any[]) => logging.error(logPrefix, ...args),
+    };
+    this.queueItemManager = new QueueItemManager({
+      logger: this.logger,
+      processorId,
+    });
+  }
+
+  async init() {
+    await this.queueItemManager.resetOutdated(QueueItemActionTypes.HASHTAG);
+  }
+
+  async pollHashtags() {
+    const { item, count } = await this.queueItemManager.getPendingHashtags(MIN_PRIORITY);
+
+    if (!item) {
+      await ProcessorManager.update(this.processorId, { lastPollAt: new Date() });
+      this.logger.debug(`No more items to go, waiting ${WAIT_TIME / 1000}s`);
+      return setTimeout(() => process.nextTick(this.pollHashtags.bind(this)), WAIT_TIME);
+    }
+
+    this.logger.info(`------- ${count} item(s) to go -------`);
+
+    const { lastEvaluatedUntilTweetId, lastEvaluatedSinceTweetId } = item?.metadata || {};
+
+    const isRequestForPreviousData = !!lastEvaluatedUntilTweetId;
+    const isRequestForNewData = !!lastEvaluatedSinceTweetId;
+    const isFirstRequest = !isRequestForPreviousData && !isRequestForNewData;
+
+    const session = undefined;
+
+    const initScraper = (retries = 3): Scraper => {
+      try {
+        const scraper = new Scraper(item.hashtag.name, {
+          resumeUntilTweetId: lastEvaluatedUntilTweetId,
+          resumeSinceTweetId: lastEvaluatedSinceTweetId,
+          nbTweetsToScrape: NB_TWEETS_TO_SCRAPE ? +NB_TWEETS_TO_SCRAPE : undefined,
+        });
+        return scraper;
+      } catch (e) {
+        if (retries - 1 >= 0) {
+          this.logger.warn(
+            `Scraper for ${item._id} (#${item.hashtag.name}) could not be processed correctly retrying again ${retries} times`
+          );
+          return initScraper(retries - 1);
+        }
+
+        throw e;
+      }
+    };
+
+    try {
+      await this.queueItemManager.startProcessingHashtag(item, {
+        previous: isRequestForPreviousData,
+        next: isRequestForNewData,
+      });
+
+      await ProcessorManager.update(this.processorId, { lastProcessedAt: new Date() });
+
+      let scraper = initScraper();
+
+      const volumetry = scraper.getVolumetry();
+
+      // const session = await mongoose.startSession();
+
+      // session.startTransaction();
+
+      // FIXME @martin should it still be used ?
+      // if (lastEvaluatedUntilTweetId) {
+      //   // This to prevent overwriting the lastEvaluatedUntilTweetId in case there is a problem
+      //   newHashtagData.metadata = { lastEvaluatedUntilTweetId };
+      // }
+
+      await HashtagVolumetryManager.batchUpsert(session)(
+        item.hashtag._id,
+        volumetry,
+        Scraper.platformId
+      );
+
+      const newHashtagData: Partial<
+        Parameters<Properties<QueueItemManager>['stopProcessingHashtag']>[2]
+      > = {};
+
+      const { id: lastProcessedUntilTweetId, date: lastProcessedTweetCreatedAt } =
+        scraper.getLastProcessedTweet() || {};
+      const { id: firstProcessedUntilTweetId } = scraper.getFirstProcessedTweet() || {};
+
+      if (isFirstRequest || isRequestForPreviousData) {
+        if (lastProcessedTweetCreatedAt) {
+          newHashtagData.oldestProcessedDate = lastProcessedTweetCreatedAt;
+        }
+
+        if (lastEvaluatedUntilTweetId !== lastProcessedUntilTweetId && lastProcessedUntilTweetId) {
+          // There might be some more data to retrieve
+          await this.queueItemManager.createHashtag(item.hashtag._id, {
+            lastEvaluatedUntilTweetId: lastProcessedUntilTweetId,
+            priority: item.priority + 1,
+          });
+
+          newHashtagData.status = HashtagStatuses.PROCESSING_PREVIOUS;
+        } else {
+          // This is the last occurence of all times
+          newHashtagData.firstOccurenceDate = lastProcessedTweetCreatedAt;
+        }
+
+        if (isFirstRequest) {
+          await this.queueItemManager.createHashtag(item.hashtag._id, {
+            lastEvaluatedSinceTweetId: firstProcessedUntilTweetId,
+            priority: QueueItemManager.PRIORITIES.HIGH,
+            processingDate: new Date(Date.now() + NEXT_PROCESS_IN_FUTURE),
+          });
+        }
+        await this.queueItemManager.stopProcessingHashtag(item, {}, newHashtagData);
+      } else if (isRequestForNewData) {
+        if (!firstProcessedUntilTweetId) {
+          // reuse same queueitem to prevent having too many of them
+          // and just change the date
+          await this.queueItemManager.stopProcessingHashtag(
+            item,
+            {
+              status: QueueItemStatuses.PENDING,
+              processingDate: new Date(Date.now() + NEXT_PROCESS_IN_FUTURE),
+              metadata: {
+                ...(item.metadata || {}),
+                numberTimesCrawled: (item.metadata.numberTimesCrawled || 0) + 1,
+              },
+            },
+            {
+              newestProcessedDate: new Date(),
+            }
+          );
+        } else {
+          await this.queueItemManager.createHashtag(item.hashtag._id, {
+            lastEvaluatedSinceTweetId: firstProcessedUntilTweetId,
+            priority: QueueItemManager.PRIORITIES.HIGH,
+            processingDate: new Date(Date.now() + NEXT_PROCESS_IN_FUTURE),
+          });
+          await this.queueItemManager.stopProcessingHashtag(
+            item,
+            {},
+            {
+              newestProcessedDate: new Date(),
+            }
+          );
+        }
+      }
+
+      // await session.commitTransaction();
+      scraper.purge();
+      this.logger.info(`Item ${item._id} processing is done, waiting ${WAIT_TIME / 1000}s`);
+    } catch (e) {
+      // await session.abortTransaction();
+      this.logger.error(e);
+
+      // we have found some volumetry
+      await this.queueItemManager.stopProcessingHashtagWithError(item, {
+        error: e.toString(),
+      });
+      this.logger.error(
+        `Item ${item._id} could not be processed correctly retrying in ${WAIT_TIME / 1000}s`
+      );
+    }
+    // session.endSession();
+
+    return setTimeout(() => {
+      return process.nextTick(this.pollHashtags.bind(this));
+    }, WAIT_TIME);
+  }
+}
--- src/managers/QueueItemManager.ts
+++ src/managers/QueueItemManager.ts
@@ -12,208 +12,231 @@ import {
 import HashtagModel from '../models/Hashtag';
 import QueueItemModel from '../models/QueueItem';
 
-export const resetOutdated = async (processorId: string) => {
-  logging.info(`reset outdated items for processorId ${processorId}`);
-  try {
-    return QueueItemModel.updateMany(
-      {
-        $or: [
-          { status: QueueItemStatuses.PROCESSING, processorId },
-          { status: QueueItemStatuses.DONE_ERROR, processorId, error: /Command failed/gim },
-        ],
-      },
-      { $set: { status: QueueItemStatuses.PENDING, processorId: null } }
-    );
-  } catch (e) {
-    logging.error(e);
-    throw new Error('Could not reset outdated queueItems');
+interface QueueItemManagerProps {
+  processorId: string;
+  logger: typeof logging;
+  session?: ClientSession;
+}
+
+export default class QueueItemManager {
+  private processorId: QueueItemManagerProps['processorId'];
+  private logger?: QueueItemManagerProps['logger'];
+  private session?: QueueItemManagerProps['session'];
+  public static PRIORITIES = {
+    NOW: 0,
+    URGENT: 1,
+    HIGH: 2,
+    MEDIUM: 3,
+  };
+  constructor({ processorId, logger, session }: QueueItemManagerProps) {
+    this.logger = logger || logging;
+    this.processorId = processorId;
+    this.session = session;
   }
-};
 
-export const PRIORITIES = {
-  NOW: 0,
-  URGENT: 1,
-  HIGH: 2,
-  MEDIUM: 3,
-};
-
-export const create = (session?: ClientSession) => async (
-  hashtag: string,
-  {
-    lastEvaluatedUntilTweetId,
-    lastEvaluatedSinceTweetId,
-    priority = PRIORITIES.NOW,
-    processingDate,
-  }: {
-    lastEvaluatedUntilTweetId?: string;
-    lastEvaluatedSinceTweetId?: string;
-    priority?: number;
-    processingDate?: Date;
-  } = {}
-) => {
-  try {
-    const queueItems = await QueueItemModel.create(
-      [
+  resetOutdated = async (action: QueueItemActionTypes) => {
+    this.logger.info(`reset outdated items for processorId ${this.processorId}`);
+    try {
+      return QueueItemModel.updateMany(
         {
-          priority,
-          action: QueueItemActionTypes.HASHTAG,
-          status: QueueItemStatuses.PENDING,
-          processingDate,
-          hashtag,
-          ...(lastEvaluatedUntilTweetId
-            ? {
-                metadata: {
-                  lastEvaluatedUntilTweetId,
-                },
-              }
-            : {}),
-          ...(lastEvaluatedSinceTweetId
-            ? {
-                metadata: {
-                  lastEvaluatedSinceTweetId,
-                },
-              }
-            : {}),
+          $or: [
+            { status: QueueItemStatuses.PROCESSING, processorId: this.processorId },
+            {
+              status: QueueItemStatuses.DONE_ERROR,
+              processorId: this.processorId,
+              error: /Command failed/gim,
+            },
+          ],
+          action,
         },
-      ],
-      session ? { session } : {}
-    );
+        { $set: { status: QueueItemStatuses.PENDING, processorId: null } }
+      );
+    } catch (e) {
+      this.logger.error(e);
+      throw new Error('Could not reset outdated queueItems');
+    }
+  };
 
-    return queueItems[0];
-  } catch (e) {
-    console.error(e);
-    throw new Error('Could not create queueItem');
-  }
-};
+  createHashtag = async (
+    hashtag: string,
+    {
+      lastEvaluatedUntilTweetId,
+      lastEvaluatedSinceTweetId,
+      priority = QueueItemManager.PRIORITIES.NOW,
+      processingDate,
+    }: {
+      lastEvaluatedUntilTweetId?: string;
+      lastEvaluatedSinceTweetId?: string;
+      priority?: number;
+      processingDate?: Date;
+    } = {}
+  ) => {
+    try {
+      const queueItems = await QueueItemModel.create(
+        [
+          {
+            priority,
+            action: QueueItemActionTypes.HASHTAG,
+            status: QueueItemStatuses.PENDING,
+            processingDate,
+            hashtag,
+            ...(lastEvaluatedUntilTweetId
+              ? {
+                  metadata: {
+                    lastEvaluatedUntilTweetId,
+                  },
+                }
+              : {}),
+            ...(lastEvaluatedSinceTweetId
+              ? {
+                  metadata: {
+                    lastEvaluatedSinceTweetId,
+                  },
+                }
+              : {}),
+          },
+        ],
+        this.session ? { session: this.session } : {}
+      );
 
-export const getPendingItems = async (
-  processorId: string,
-  minPriority: number = PRIORITIES.NOW
-) => {
-  logging.debug(`get PENDING items`);
-  try {
-    const query: FilterQuery<QueueItem> = {
-      status: QueueItemStatuses.PENDING,
-      processingDate: { $lte: new Date() },
-      priority: { $gte: minPriority },
-    };
-    const count = await QueueItemModel.find(query).countDocuments();
+      return queueItems[0];
+    } catch (e) {
+      this.logger.error(e);
+      throw new Error('Could not create queueItem');
+    }
+  };
 
-    const item: QueueItem = await QueueItemModel.findOneAndUpdate(
-      query,
-      {
-        $set: { status: QueueItemStatuses.PROCESSING, processorId },
-      },
-      {
-        setDefaultsOnInsert: true,
-        sort: { priority: 1, _id: -1 },
-      }
-    ).populate('hashtag');
+  getPendingHashtags = async (minPriority: number = QueueItemManager.PRIORITIES.NOW) => {
+    this.logger.debug(`get PENDING items`);
+    try {
+      const query: FilterQuery<QueueItem> = {
+        status: QueueItemStatuses.PENDING,
+        action: QueueItemActionTypes.HASHTAG,
+        processingDate: { $lte: new Date() },
+        priority: { $gte: minPriority },
+      };
 
-    return {
-      item,
-      count,
-    };
-  } catch (e) {
-    console.error(e);
-    throw new Error('Could not create queueItem');
-  }
-};
+      const count = await QueueItemModel.find(query).countDocuments();
 
-export const startProcessing = async (
-  item: QueueItem,
-  processorId: string,
-  { previous, next }: { previous?: boolean; next?: boolean }
-) => {
-  logging.debug(
-    `Start processing for queueItem ${item._id} (pr:${item.priority}) and processor ${processorId}`
-  );
-  try {
-    await QueueItemModel.updateOne(
-      { _id: item._id },
-      { $set: { status: QueueItemStatuses.PROCESSING, processorId } }
-    );
-    await HashtagModel.updateOne(
-      { _id: item.hashtag },
-      {
-        $set: {
-          status:
-            HashtagStatuses[
-              previous
-                ? HashtagStatuses.PROCESSING_PREVIOUS
-                : next
-                ? HashtagStatuses.PROCESSING_NEW
-                : HashtagStatuses.PROCESSING
-            ],
+      const item: QueueItem = await QueueItemModel.findOneAndUpdate(
+        query,
+        {
+          $set: { status: QueueItemStatuses.PROCESSING, processorId: this.processorId },
         },
-      }
-    );
-  } catch (e) {
-    console.error(e);
-    throw new Error(
-      `Could not start processing for queueItem ${item._id} and processor ${processorId}`
-    );
-  }
-};
+        {
+          setDefaultsOnInsert: true,
+          sort: { priority: 1, _id: -1 },
+        }
+      ).populate('hashtag');
 
-export const stopProcessing = (session: ClientSession) => async (
-  item: QueueItem,
-  itemData: Partial<QueueItem>,
-  hashtagData: Partial<Hashtag>
-) => {
-  logging.debug(`Stop processing for queueItem ${item._id} and processor ${itemData.processorId}`);
-  try {
-    await QueueItemModel.updateOne(
-      { _id: item._id },
-      {
-        $set: {
-          status: QueueItemStatuses.DONE,
-          ...itemData,
+      return {
+        item,
+        count,
+      };
+    } catch (e) {
+      console.error(e);
+      throw new Error('Could not create queueItem');
+    }
+  };
+
+  startProcessingHashtag = async (
+    item: QueueItem,
+    { previous, next }: { previous?: boolean; next?: boolean }
+  ) => {
+    this.logger.debug(
+      `Start processing for queueItem ${item._id} (pr:${item.priority}) and processor ${this.processorId}`
+    );
+    try {
+      await QueueItemModel.updateOne(
+        { _id: item._id },
+        { $set: { status: QueueItemStatuses.PROCESSING, processorId: this.processorId } }
+      );
+      await HashtagModel.updateOne(
+        { _id: item.hashtag },
+        {
+          $set: {
+            status:
+              HashtagStatuses[
+                previous
+                  ? HashtagStatuses.PROCESSING_PREVIOUS
+                  : next
+                  ? HashtagStatuses.PROCESSING_NEW
+                  : HashtagStatuses.PROCESSING
+              ],
+          },
+        }
+      );
+    } catch (e) {
+      this.logger.error(e);
+      throw new Error(
+        `Could not start processing for queueItem ${item._id} and processor ${this.processorId}`
+      );
+    }
+  };
+
+  stopProcessingHashtag = async (
+    item: QueueItem,
+    itemData: Partial<QueueItem>,
+    hashtagData: Partial<Hashtag>
+  ) => {
+    this.logger.debug(
+      `Stop processing for queueItem ${item._id} and processor ${this.processorId}`
+    );
+    try {
+      await QueueItemModel.updateOne(
+        { _id: item._id },
+        {
+          $set: {
+            status: QueueItemStatuses.DONE,
+            processorId: this.processorId,
+            ...itemData,
+          },
         },
-      },
-      session ? { session } : {}
-    );
+        this.session ? { session: this.session } : {}
+      );
 
-    await HashtagModel.updateOne(
-      { _id: item.hashtag },
-      { $set: { status: HashtagStatuses.DONE, error: null, ...hashtagData } },
-      session ? { session } : {}
+      await HashtagModel.updateOne(
+        { _id: item.hashtag },
+        { $set: { status: HashtagStatuses.DONE, error: null, ...hashtagData } },
+        this.session ? { session: this.session } : {}
+      );
+    } catch (e) {
+      this.logger.error(e);
+      throw new Error(
+        `Could not stop processing for queueItem ${item._id} and processor ${this.processorId}`
+      );
+    }
+  };
+  stopProcessingHashtagWithError = async (item: QueueItem, hashtagData: Partial<Hashtag>) => {
+    this.logger.debug(
+      `Stop processing with error for queueItem ${item._id} and processor ${this.processorId}`
     );
-  } catch (e) {
-    console.error(e);
-    throw new Error(
-      `Could not stop processing for queueItem ${item._id} and processor ${itemData.processorId}`
-    );
-  }
-};
+    try {
+      await QueueItemModel.updateOne(
+        { _id: item._id },
+        {
+          $set: {
+            status: QueueItemStatuses.DONE_ERROR,
+            processorId: this.processorId,
+            error: hashtagData.error,
+          },
+        },
+        this.session ? { session: this.session } : {}
+      );
 
-export const stopProcessingWithError = (session: ClientSession) => async (
-  item: QueueItem,
-  processorId: string,
-  hashtagData: Partial<Hashtag>
-) => {
-  logging.debug(
-    `Stop processing with error for queueItem ${item._id} and processor ${processorId}`
-  );
-  try {
-    await QueueItemModel.updateOne(
-      { _id: item._id },
-      { $set: { status: QueueItemStatuses.DONE_ERROR, processorId, error: hashtagData.error } },
-      session ? { session } : {}
-    );
+      await HashtagModel.updateOne(
+        { _id: item.hashtag },
+        { $set: { status: HashtagStatuses.DONE_ERROR, ...hashtagData } },
+        this.session ? { session: this.session } : {}
+      );
 
-    await HashtagModel.updateOne(
-      { _id: item.hashtag },
-      { $set: { status: HashtagStatuses.DONE_ERROR, ...hashtagData } },
-      session ? { session } : {}
-    );
-
-    // TODO
-    // Send email
-  } catch (e) {
-    console.error(e);
-    throw new Error(
-      `Could not stop processing with error for queueItem ${item._id} and processor ${processorId}`
-    );
-  }
-};
+      // TODO
+      // Send email
+    } catch (e) {
+      console.error(e);
+      throw new Error(
+        `Could not stop processing with error for queueItem ${item._id} and processor ${this.processorId}`
+      );
+    }
+  };
+}
