63f7d40cb715067ab03c9fdd243dfab6fe0a440e
Martin Ratinaud <martinratinaud@gmail.com>
2021-07-13 07:29:40 +0200

    chore: add way to pass options to execSync

--- src/common/cmd-utils.ts
+++ src/common/cmd-utils.ts
@@ -1,13 +1,13 @@
 import * as logging from 'common/logging';
 
-import { execSync } from 'child_process';
+import { ExecSyncOptionsWithBufferEncoding, execSync } from 'child_process';
 
-export const execCmd = (cmd: string): string => {
+export const execCmd = (cmd: string, options: ExecSyncOptionsWithBufferEncoding = {}): string => {
   logging.debug(`[CMD] ${cmd}`);
   const start = process.hrtime();
 
   // https://stackoverflow.com/questions/63796633/spawnsync-bin-sh-enobufs
-  const result = execSync(cmd, { stdio: 'pipe' });
+  const result = execSync(cmd, { stdio: 'pipe', ...options });
 
   const end = process.hrtime(start);
   logging.debug(`[CMD] Command completed in ${Number(end[1] / 1000000).toFixed(0)}ms`);
