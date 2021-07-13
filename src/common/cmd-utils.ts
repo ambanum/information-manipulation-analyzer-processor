e9151d0919c0e40cc052b52283bb8c0a68be23fb
Martin Ratinaud <martinratinaud@gmail.com>
2021-07-13 07:56:04 +0200

    fix: try to prevent Error: spawnSync /bin/sh E2BIG

--- src/common/cmd-utils.ts
+++ src/common/cmd-utils.ts
@@ -8,7 +8,6 @@ export const execCmd = (cmd: string, options: ExecSyncOptionsWithBufferEncoding 
 
   // https://stackoverflow.com/questions/63796633/spawnsync-bin-sh-enobufs
   const result = execSync(cmd, { stdio: 'pipe', ...options });
-
   const end = process.hrtime(start);
   logging.debug(`[CMD] Command completed in ${Number(end[1] / 1000000).toFixed(0)}ms`);
 
